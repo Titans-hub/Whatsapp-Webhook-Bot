4f93fd5690fbbde355ea26393060c6840deb1971
Titans-hub <abinesh25py@gmail.com>
2025-12-06 21:04:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 
-const VERIFY_TOKEN = "titans123"; // same as WhatsApp dashboard
+const VERIFY_TOKEN = "titans123"; 
 
 app.use(express.json());
 
 // GET - webhook verification + test
 app.get("/", (req, res) => {
-  console.log("GET / webhook hit. Query:", req.query);
-
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
+
+  console.log("GET / webhook hit. Query:", req.query);
+  console.log("SERVER VERIFY_TOKEN:", "${VERIFY_TOKEN}");
+  console.log("TOKEN FROM META:", "${token}");
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
     console.log("WEBHOOK VERIFIED OK");
@@ -26,13 +28,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log("POST / webhook hit. Incoming Message:");
   console.log(JSON.stringify(req.body, null, 2));
-
-  // Always reply 200 fast
   return res.sendStatus(200);
 });
 
-// IMPORTANT: use Render's PORT
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log("WhatsApp webhook running on port" + PORT);
+  console.log("WhatsApp webhook running on port " + PORT);
 });
